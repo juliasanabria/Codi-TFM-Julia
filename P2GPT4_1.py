6f266491195513f55982d07ad81f1b7346bed6b5
juliasanabria <94184933+juliasanabria@users.noreply.github.com>
2023-06-17 19:48:15 +0200

    Update P2GPT4_1.py

--- P2GPT4_1.py
+++ P2GPT4_1.py
@@ -7,56 +7,48 @@ Màster en Bioinformàtica i Bioestadística
 """
 
 import os
-import glob
+
 import numpy as np
-import matplotlib.pyplot as plt
+
 from skimage import io, color, measure
 from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 import time
 
 start_time = time.time()
-def analisi_nuclis(input_dir, output_dir):
-    tiff_files = glob.glob(os.path.join(input_dir, '*.tif'))
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+input_dir = '/Users/juliasanabriafranquesa/Desktop/UOC/Q4/Q4TFM/ImatgesOriginals_Proves/A (1).tif'
+output_dir = '/Users/juliasanabriafranquesa/Desktop/UOC/Q4/Q4TFM/Resultats Julia/P2GPT4'
 
-    for tiff_file in tiff_files:
-        image = io.imread(tiff_file)
-        image_gray = color.rgb2gray(image)
-        thresh = threshold_otsu(image_gray)
-        binary = image_gray < thresh
+image = io.imread(input_dir)
+image_gray = color.rgb2gray(image)
+thresh = threshold_otsu(image_gray)
+binary = image_gray < thresh
 
-        binary_clear = clear_border(binary)
-        labels = measure.label(binary_clear)
-        regions = measure.regionprops(labels)
+binary_clear = clear_border(binary)
+labels = measure.label(binary_clear)
+regions = measure.regionprops(labels)
         
-        area_total = 0
-        area_nuclis = 0
+area_total = 0
+area_nuclis = 0
 
-        for region in regions:
-            area_total += region.area
-            if np.mean(image[labels == region.label]) < 100:
-                area_nuclis += region.area
-                for coord in region.coords:
-                    image[coord[0], coord[1]] = (255, 0, 0)
-
-
-
-        output_file = os.path.join(output_dir, os.path.basename(tiff_file))
-        plt.savefig(output_file, dpi=300, bbox_inches='tight')
-
-        ratio = area_nuclis / area_total * 100
-        print(f"Imatge: {tiff_file}, Àrea total: {area_total}, Àrea nuclis: {area_nuclis}, Percentatge: {ratio:.2f}%")
-
-if __name__ == '__main__':
-    input_dir = '/Users/juliasanabriafranquesa/Desktop/UOC/Q4/Q4TFM/ImatgesOriginals_Proves'
-    output_dir = '/Users/juliasanabriafranquesa/Desktop/UOC/Q4/Q4TFM/Resultats Julia/P2GPT4'
-    analisi_nuclis(input_dir, output_dir)
-    
+for region in regions:
+    area_total += region.area
+    if np.mean(image[labels == region.label]) < 100:
+        area_nuclis += region.area
+        for coord in region.coords:
+            image[coord[0], coord[1]] = (255, 0, 0)
 
 
+
+output_file = os.path.join(output_dir, os.path.basename(output_dir))
+tiff_file = "A (1)"
+
+ratio = area_nuclis / area_total * 100
+print(f"Imatge: {tiff_file}, Àrea total: {area_total}, Àrea nuclis: {area_nuclis}, Percentatge: {ratio:.2f}%")
+
+output_path = os.path.join(output_dir, "output_image.tif")
+io.imsave(output_path, image)
 
 end_time = time.time()
 execution_time = end_time - start_time
